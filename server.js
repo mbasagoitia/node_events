6335d7213f808f9df948e774f0c6b3c5164d1588
basagoitiam <marika.basagoitia@gmail.com>
2023-01-18 14:18:01 -0500

    finished setting up server and form

--- server.js
+++ server.js
@@ -1,1 +1,53 @@
 // Complete Events Exercise
+const { EventEmitter } = require("events");
+const path = require("path");
+const http = require("http");
+const fs = require("fs");
+
+const NewsLetter = new EventEmitter();
+
+const server = http.createServer((req, res) => {
+    const chunks = [];
+
+    req.on("data", (chunk) => {
+        chunks.push(chunk);
+    });
+
+    req.on("end", () => {
+        if (req.url == "/newsletter_signup" && req.method == "GET") {
+            res.writeHead(200, { "content-type": "text/html" });
+            fs.readFile("signup_form.html", (err, contents) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    res.write(contents);
+                    res.end();
+                }
+            })
+        }
+        else if (req.url == "/newsletter_signup" && req.method == "POST") {
+            let reqBody = Buffer.concat(chunks).toString();
+            NewsLetter.emit("signup", reqBody);
+            res.writeHead(200, { "content-type": "text/html" });
+            res.write("Successfully signed up!");
+            res.end();
+        } else {
+            res.writeHead(404, { "content-type": "text/html" });
+            res.write("The page you are looking for cannot be found.");
+            res.end();
+        }
+    })
+
+}).listen(3000, () => {
+    console.log("Listening on port 3000...");
+});
+
+NewsLetter.on("signup", (contact) => {
+    fs.appendFile("contact-info.csv", `\n${contact}`, (err) => {
+        if (err) {
+            console.error(err);
+        }
+        console.log("Added new contact to contact-info.csv");
+    })
+})
+
